b8061834ff5c6edd204d798fae4ea1908d0bfe0b
ariefminardi <ariefminardi140@gmail.com>
2026-02-19 14:57:17 +0700

    error handling

--- src/flow/control_flow.rs
+++ src/flow/control_flow.rs
@@ -32,3 +32,27 @@ pub fn flow() {
     };
     println!("Multiple: {}", age_range);
 }
+
+/* ERROR HANDLING
+ * - Result<T, E>: dipakai untuk operasi yang bisa jalan dan tidak
+ *                 dipakai bersama `Ok()` dan `Err()`
+ * - Option<T>: dipakai untuk operasi yang bisa ada atau none
+ *              pakai `Some()` kalau ada nilainya dan `None` kalau tidak ada
+ * - .unwrap(): kalo ada datanya ambil tapi kalo ga ada crash
+ * - .expect(): sama kaya unwrap tapi bisa dikasih pesan costom
+ */
+pub fn result_handling(angka1: i16, angka2: i16) -> Result<i16, String> {
+    if angka1 == 0 && angka2 == 0 {
+        Err(String::from("kedua nya tidak boleh nol"))
+    } else {
+        Ok(angka1 + angka2)
+    }
+}
+
+pub fn option_handling(user: &str) -> Option<String> {
+    if user.is_empty() {
+        None
+    } else {
+        Some("option running".to_string())
+    }
+}
